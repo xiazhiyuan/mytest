b601a281cb1e7c1c4e92e5691cf04838233edc17
zhiyuanxia <356701758@qq.com>
2016-03-25 16:22:38 +0800

    增加一些线程测试代码

---
+++ src/main/java/test/concurrent/exchanger/core/Main.java
@@ -0,0 +1,45 @@
+package test.concurrent.exchanger.core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Exchanger;
+
+import test.concurrent.exchanger.task.Consumer;
+import test.concurrent.exchanger.task.Producer;
+
+
+
+/**
+ * Main class of the example
+ *
+ */
+public class Main {
+
+	/**
+	 * Main method of the example
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Creates two buffers
+		List<String> buffer1=new ArrayList<>();
+		List<String> buffer2=new ArrayList<>();
+		
+		// Creates the exchanger
+		Exchanger<List<String>> exchanger=new Exchanger<>();
+		
+		// Creates the producer
+		Producer producer=new Producer(buffer1, exchanger);
+		// Creates the consumer
+		Consumer consumer=new Consumer(buffer2, exchanger);
+		
+		// Creates and starts the threads
+		Thread threadProducer=new Thread(producer);
+		Thread threadConsumer=new Thread(consumer);
+		
+		threadProducer.start();
+		threadConsumer.start();
+
+	}
+
+}
